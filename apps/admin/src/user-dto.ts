ccf6fb509b3c332b0464e560b9d691caabe011da
xcr <it_xcr@163.com>
2020-09-22 00:18:07 +0800

    完善一丢丢

--- apps/admin/src/user-dto.ts
+++ apps/admin/src/user-dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserDto {
-  readonly username: string;
+  @ApiProperty({
+    description: '用户名',
+    example: 'user'
+  })
+  readonly username: string | number;
+  @ApiProperty({
+    description: '密码',
+    example: 'pass'
+  })
   readonly password: string;
 }
--- apps/admin/src/users/users.controller.ts
+++ apps/admin/src/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Crud } from 'nestjs-mongoose-crud';
 import { ApiTags } from '@nestjs/swagger';
 import { InjectModel } from 'nestjs-typegoose';
 import { UserModel } from '@libs/db/models/user.model';
 import { UserDto } from '../user-dto';
+
+// const jwt = require('jsonwebtoken');
 
 @Crud({
   model: UserModel,
@@ -17,38 +19,67 @@ export class UsersController {
   ) {
   }
 
+  // const auth = async (author, id, next) => {
+  //   console.log(author);
+  //   console.log(id);
+  //   console.log(next);
+  // }
+
+
   @Post('')
   async getAll() {
     return {
-      code: 404,
-      message: '啥都没有，不给你看'
-    }
+      status: 404,
+      message: '啥都没有，不给你看',
+    };
   }
 
   @Post('register')
   async post(@Body() userDto: UserDto) {
     const user = await this.model.findOne({
-      username: userDto.username
+      username: userDto.username,
     });
     if (user) {
       return {
-        code: 422,
+        status: 422,
         message: `用户名已存在， 请重新输入`,
       };
     }
-    return await this.model(userDto).save();
+    await this.model(userDto).save();
+    return {
+      status: 200,
+      message: '注册成功',
+    };
   }
 
   @Post('login')
   async login(@Body() userDto: UserDto) {
-    const user = await this.model.findOne(userDto);
-    console.log(user);
-    // if (user) {
-    //   return {
-    //     code: 422,
-    //     message: `用户名已存在， 请重新输入`
-    //   };
-    // }
-    // return await this.model(userDto).save()
+    const userName = await this.model.findOne({
+      username: userDto.username,
+    });
+    if (!userName) {
+      return {
+        status: 422,
+        message: '用户名不存在',
+      };
+    }
+    const passWd = await this.model.findOne({
+      password: userDto.password,
+    });
+
+    if (!passWd) {
+      return {
+        status: 422,
+        message: '密码错误',
+      };
+    }
+
+    // 生产token
+    // const token = jwt.sign({ id: passWd._id }, process.env.SECRET);
+    return {
+      status: 200,
+      // token: token,
+      id: passWd._id
+    };
   }
 }
